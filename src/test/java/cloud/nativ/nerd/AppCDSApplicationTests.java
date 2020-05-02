e57b17e9abaee9995c3a183ad6085fb12e8db978
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-05-02 21:45:50 +0200

    Added Dockerfile for AppCDS execution. Generate class list during test and boot run.

--- src/test/java/cloud/nativ/nerd/AppCDSApplicationTests.java
+++ src/test/java/cloud/nativ/nerd/AppCDSApplicationTests.java
@@ -1,11 +1,9 @@
 package cloud.nativ.nerd;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class AppCDSApplicationTests {
 
     @Test
